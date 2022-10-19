d99898634d574ff63c8e8189790e8fc7b776d0bf
salty-muffin <t.babel@gmx.com>
2022-10-19 15:45:52 -0600

    conflated markdown and tag-selector into markdown

--- src/lib/components/markdown.svelte
+++ src/lib/components/markdown.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-	import TagSelector from './tag-selector.svelte';
-
 	import type { Hast } from '$lib/types';
 
 	export let hast: Hast | undefined;
 </script>
 
 {#if hast && hast.children}
-	<TagSelector children={hast.children} />
+	{#each hast.children as child}
+		{#if child.type === 'element'}
+			{#if child.tagName === 'img'}
+				<img
+					src={child.properties?.src}
+					alt={child.properties?.alt}
+					width={child.properties?.width}
+					height={child.properties?.height}
+				/>
+			{:else if child.tagName === 'a'}
+				<a href={child.properties?.href}><svelte:self hast={child} /></a>
+			{:else}
+				<svelte:element this={child.tagName}>
+					<svelte:self hast={child} />
+				</svelte:element>
+			{/if}
+		{:else if child.type === 'text'}
+			{child.value}
+		{/if}
+	{/each}
 {/if}
