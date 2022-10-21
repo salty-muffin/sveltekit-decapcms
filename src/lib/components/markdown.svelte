a3bd58ec748d6768c8cbf98ab6c910ac32ec51b7
salty-muffin <t.babel@gmx.com>
2022-10-20 20:26:27 -0700

    cleaned up

--- src/lib/components/markdown.svelte
+++ src/lib/components/markdown.svelte
@@ -2,7 +2,6 @@
 	import Image from '$lib/components/image.svelte';
 
 	import type { Hast, ImageOptions } from '$lib/types';
-	import { image } from 'mdast-util-to-hast/lib/handlers/image';
 
 	export let hast: Hast | undefined;
 	export let imageOptions: ImageOptions;
@@ -11,16 +10,16 @@
 {#if hast && hast.children}
 	{#each hast.children as child}
 		{#if child.type === 'element'}
-			{#if child.tagName === 'img'}
+			{#if child.tagName === 'img' && child.properties}
 				<Image
-					src={child.properties?.src}
-					alt={child.properties?.alt}
-					width={child.properties?.width}
-					height={child.properties?.height}
+					src={child.properties.src}
+					alt={child.properties.alt}
+					width={child.properties.width}
+					height={child.properties.height}
 					options={imageOptions}
 				/>
-			{:else if child.tagName === 'a'}
-				<a href={child.properties?.href}><svelte:self hast={child} {imageOptions} /></a>
+			{:else if child.tagName === 'a' && child.properties}
+				<a href={child.properties.href}><svelte:self hast={child} {imageOptions} /></a>
 			{:else}
 				<svelte:element this={child.tagName}
 					><svelte:self hast={child} {imageOptions} /></svelte:element
