9a9efde073e63555589dcd5f624949780e19a0df
salty-muffin <t.babel@gmx.com>
2024-07-29 14:41:46 +0200

    changed adapter setting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,7 @@ const config = {
 		}
 	}),
 	kit: {
-		adapter: adapter({fallback: '404.html', strict: false}),
+		adapter: adapter(),
 		prerender: {
 			entries: ['/']
 		}
